14050a85174101e47664998a90c88a96ce909ff9
Nail Minnigalin <nail160306@gmail.com>
2021-03-24 20:10:42 +0300

    Add some comments

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import datetime
 
 if __name__ == '__main__':
+    # Input
     name = input("Enter your name: ")
     age = int(input("Enter your age: "))
     number = int(input("Enter some number: "))
 
+    # Processing
     now = datetime.datetime.now()
     years_to_hundred = 100 - age
 
+    # Output
     for i in range(number):
         print("Hi! " + name)
         print("You will turn 100 years old in " + str(now.year + years_to_hundred) + " year")
